5a608e484d4b21cb32fe49e4a987ffc89584d1d4
Jafeth Bendezu <jafeth.bendezu.a@gmail.com>
2018-02-16 23:10:09 -0500

    modificaciones hechas el: 2018-02-16 . 23:10:09

--- app/DoctrineMigrations/Version20180217031403.php
+++ app/DoctrineMigrations/Version20180217031403.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180216184901 extends AbstractMigration
+class Version20180217031403 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
