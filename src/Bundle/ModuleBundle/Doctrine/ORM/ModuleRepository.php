16b378e2adb05965074dc00b4d4beccc7b6cc415
Tianos <tianos@gmail.com>
2019-07-17 14:16:49 -0500

    manage

--- src/Bundle/ModuleBundle/Doctrine/ORM/ModuleRepository.php
+++ src/Bundle/ModuleBundle/Doctrine/ORM/ModuleRepository.php
@@ -13,7 +13,7 @@ class ModuleRepository extends TianosEntityRepository implements ModuleRepositor
     /**
      * {@inheritdoc}
      */
-    public function find($id)
+	public function find($id, $lockMode = NULL, $lockVersion = NULL)
     {
         $em = $this->getEntityManager();
         $dql = "
